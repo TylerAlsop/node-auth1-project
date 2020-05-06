160a610228193d730318b82050bad926203e0b5f
Tyler Alsop <tylerjalsop@gmail.com>
2020-05-05 19:47:44 -0600

    Installed dependencies. Setup the server in package.json, index.js, and server.js.

--- README.md
+++ README.md
@@ -28,3 +28,19 @@ Add support for **sessions** and **cookies**, use them to keep a record of logge
 
 - Write a piece of **global** middleware that ensures a user is logged in when accessing _any_ route prefixed by `/api/users`.
 - Build a React application that implements components to register, login and view a list of users. Gotta keep sharpening your React skills.
+
+
+
+
+
+
+## Notes for installing dependencies:
+npm init -y
+npm install --save-dev nodemon
+npm install knex --save sqlite3
+npx knex init
+npm install express
+npm install helmet
+npx knex migrate:make initial
+npm install bcryptjs
+npm install express-session
---
+++ api/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session")
+
+// const usersRouter = require("../users/users-router.js");
+// const authRouter = require("../auth/authRouter")
+// const restricted = require("../auth/restricted_middleware")
+
+const server = express();
+
+const sessionConfig = {
+  name: "Cookie Monster",
+  secret: "Keep it secret, keep it safe!",
+  cookie: {
+    // This is information for both the cookie and the session
+    maxAge: 1000 * 60 * 60,
+    secure: false, // Should be true during prodeuction. To send only over https
+    httpOnly: true, // true means no access from JS
+
+  },
+  resave: false,
+  saveUninitialized: true, // GDPR laws require to check with client 
+  ///*** I think the teacher said that if the client chooses not to accept you collecting cookies then this value needs to be changed to false.
+
+}
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig))
+
+// server.use("/api/users", restricted, usersRouter);
+// server.use("/api/auth", authRouter);
+
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+
+module.exports = server;
